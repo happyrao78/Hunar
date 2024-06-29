d0f05a609fa1c1601d7d4390257528e310825db2
Titan-Codes <vedansh1sharma2005@gmail.com>
2024-06-29 14:33:57 +0530

    added vite proxy

--- newsfetch/vite.config.js
+++ newsfetch/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api': 'https://newsapi.org/'
+    }
+  },
   plugins: [react()],
   build:{
     outDir: 'dist',
