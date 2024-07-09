2e9cc0711ac72046d56ec4dec74628ec382e047e
HAPPY YADAV <happy.hy0017@gmail.com>
2024-07-09 14:17:59 +0530

    updated font

--- newsfetch/src/components/Header/Header.jsx
+++ newsfetch/src/components/Header/Header.jsx
@@ -73,7 +73,7 @@ function Header() {
               </div>
                 <button
                   onClick={handleLogin}
-                  className="text-gray-800 dark:text-white dark:hover:bg-blue-400 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm md:text-lg lg:text-xl px-2 md:px-4 py-1.5 md:py-2 lg:py-2.5 mr-1 md:mr-2 focus:outline-none dark:transition ease-in-out duration-500"
+                  className="text-gray-800 dark:text-white dark:hover:bg-blue-400  hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xl md:text-lg lg:text-xl px-2 md:px-4 py-1.5 md:py-2 lg:py-2.5 mr-1 md:mr-2 focus:outline-none dark:hover:transition ease-in-out duration-500"
                 >
                   Log in
                 </button>
@@ -88,7 +88,7 @@ function Header() {
             )}
             <button
               onClick={() => setIsMenuOpen(!isMenuOpen)}
-              className="lg:hidden text-gray-800 ml-2"
+              className="lg:hidden text-gray-800 ml-2 dark:text-white dark:transition ease-in-out duration-500"
             >
               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
